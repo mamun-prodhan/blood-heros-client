a430c219fecaaeff1fd94271ec9cba469bce8e02
mamun-prodhan <mamunpr9@gmail.com>
2023-11-26 22:03:32 +0600

    why should you donate completed

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import banner from "../../../assets/banner.png";
 import { Link } from "react-router-dom";
 const Banner = () => {
   return (
-    <div className="flex flex-col gap-5 items-center md:flex-row-reverse">
+    <div className="flex pb-5 flex-col gap-5 items-center md:flex-row-reverse">
       <div className="flex-1">
         <img src={banner} alt="" />
       </div>
