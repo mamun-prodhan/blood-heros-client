afe7a2dd29a401f7c5b94494a319b4232c2a0d80
mamun-prodhan <mamunpr9@gmail.com>
2023-11-27 19:15:48 +0600

    error page implemented

--- src/Pages/Shared/ErrorPage/ErrorPage.jsx
+++ src/Pages/Shared/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+import errorImg from "../../../assets/404.gif";
+
 const ErrorPage = () => {
   return (
-    <div>
-      <h2>This is error page</h2>
+    <div className="my-20">
+      <div className="mt-20">
+        <img className="mx-auto" src={errorImg} alt="" />
+      </div>
+      <h2 className="text-2xl md:text-4xl font-bold text-[#FF6251] text-center my-6">
+        Page Not Found
+      </h2>
+      <div className="text-center">
+        <Link to="/">
+          <button className=" text-base md:text-xl font-bold px-4 md:px-7 py-3 md:py-4 duration-200 bg-[#FF6251] hover:bg-[#fc422e] text-white rounded-lg">
+            Go Back To Home
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
